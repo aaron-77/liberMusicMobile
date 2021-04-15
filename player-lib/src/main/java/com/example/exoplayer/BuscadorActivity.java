8f96c38c481fabb7074631c208dde7b1fe7140e5
aaron hernandez lara <aaronhl1313@gmail.com>
2021-04-15 13:48:08 -0500

    comentario de prueba para lint

--- player-lib/src/main/java/com/example/exoplayer/BuscadorActivity.java
+++ player-lib/src/main/java/com/example/exoplayer/BuscadorActivity.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 
 public class BuscadorActivity extends AppCompatActivity {
 
+    //comentario de prueba para lint
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
